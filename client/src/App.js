0a260f25ec78f16cb799d65aabe7939ee35db6ab
BellaBradbury <bellabradbury@yahoo.com>
2023-02-07 12:25:26 -0500

    header created & linked

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = props => {
+    return(
+        <div className='wrap header--flex'>
+            <h1 className='header--logo'><NavLink to="/"></NavLink></h1>
+            <nav>
+                <ul className='header--signedout'>
+                    <li><NavLink to="/signup"></NavLink></li>
+                    <li><NavLink to="/signin"></NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
+
