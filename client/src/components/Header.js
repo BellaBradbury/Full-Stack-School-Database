7964237f1473bfe99defceacb41fe1a1c71451d5
BellaBradbury <bellabradbury@yahoo.com>
2023-02-13 13:02:30 -0500

    header.js -> v6

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class Header extends React.PureComponent {
-    render() {
-        const {context} = this.props;
-        const authUser = context.authenticatedUser;
+import { UserContext } from '../Context';
 
-        return(
-            <header>
-                <div className='wrap header--flex'>
-                <h1 className='header--logo'><NavLink to="/">Courses</NavLink></h1>
-                    <nav>
-                        <ul className='header--signedout'>
-                            {authUser ? (
-                                <React.Fragment>
-                                    <li><span>Welcome, {authUser.name}!</span></li>
-                                    <li><NavLink to='/signout'>Sign Out</NavLink></li>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    <li><NavLink to="/signup">Sign Up</NavLink></li>
-                                    <li><NavLink to="/signin">Sign In</NavLink></li>
-                                </React.Fragment>
-                            )}
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-        );
-    }
+const Header = () => {
+    const authUser = useContext(UserContext);
+
+    return(
+        <header>
+            <div className='wrap header--flex'>
+                <h1 className='header--logo'><NavLink to="/courses">Courses</NavLink></h1>
+                <nav>
+                    {authUser ? (
+                        <React.Fragment>
+                            <ul className='header--signedin'>
+                                <li>Welcome, {authUser.name}!</li>
+                                <li><NavLink to='/signout'>Sign Out</NavLink></li>
+                            </ul>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <ul className='header--signedout'>
+                                <li><NavLink to="/signup">Sign Up</NavLink></li>
+                                <li><NavLink to="/signin">Sign In</NavLink></li>
+                            </ul>
+                        </React.Fragment>
+                    )}
+                </nav>
+            </div>
+        </header>
+    );
 };
 
+export default Header;
