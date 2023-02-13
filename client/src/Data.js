002034d600818f09108ed1776cde2baa0ee42a3c
BellaBradbury <bellabradbury@yahoo.com>
2023-02-13 15:44:31 -0500

    added courses to data

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,6 +1,9 @@
+// VARIABLES
 import config from './Config';
 
+// 
 export default class Data {
+    // defines the params for GET requests
     api(path, method = 'GET', body = null, requiresAuth = false, credentials = null) {
         const url = config.apiBaseUrl + path;
 
@@ -15,6 +18,7 @@ export default class Data {
             options.body = JSON.stringify(body);
         }
 
+        // encodes user credentials
         if (requiresAuth) {
             const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
             options.headers['Authorization'] = `Basic ${encodedCredentials}`;
@@ -23,6 +27,7 @@ export default class Data {
         return fetch(url, options);
     }
 
+    // GET USER from database
     async getUser(username, password) {
         const response = await this.api('/user', 'GET', null, true, {username, password});
         if (response.status === 200) {
@@ -34,6 +39,7 @@ export default class Data {
         }
     }
 
+    // POST USER to database
     async createUser(user) {
         const response = await this.api('/users', 'POST', user);
         if (response.status === 201) {
@@ -42,6 +48,26 @@ export default class Data {
             return response.json().then(data => {
                 return data.errors;
             });
+        } else {
+            throw new Error();
+        }
+    }
+
+    // POST COURSES to database
+    async createCourse(path, method, body, username, password) {
+        const response = await this.api(path, method, body, true, {username, password});
+        if (response.status === 201) {
+            console.log('You have created a course.');
+        } else {
+            throw new Error();
+        }
+    }
+
+    // PUT COURSES to update database
+    async updateCourse(path, method, body, username, password) {
+        const response = await this.api(path, method, body, true, {username, password});
+        if (response.status === 204) {
+            console.log('You have updated a course.');
         } else {
             throw new Error();
         }
