20d6587675b5f7c5f6a8799c49d9ce76c48d50b5
BellaBradbury <bellabradbury@yahoo.com>
2023-02-15 15:20:26 -0500

    html added for courses

---
+++ client/src/components/Courses.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import config from '../Config';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+export default function Courses({ history }) {
+    const [coursesData, setCourses] = useState([]);
+
+    const coursesFind = async () => {
+        await axios.get(config.apiBaseUrl + '/courses')
+                   .then((res) => {
+                    setCourses(res.data.courses);
+                   })
+                   .catch((err) => {
+                    history.push({
+                        pathname: '/errors', 
+                        state: {error: err.message}});
+                   });
+    }
+    coursesFind();
+
+    const courseTitles = coursesData.map((course) => {
+        return (
+            <Link className='course--module course--link' to={`/courses/${course.id}`} key={course.id}>
+                <h2 className='course--label'>Course</h2>
+                <h3 className='course--title'>{course.title}</h3>
+            </Link>
+        );
+    });
+
+    return (
+        <main>
+            <div className='wrap main--grid'>
+                {courseTitles}
+                <Link className='course--module course--add-module' to='/courses/create'>
+                    <span className='course--add--title'>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                         <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                        </svg>
+                    New Course </span>
+                </Link>
+            </div>
+        </main>
+    )
+}
---
+++ client/src/components/CreateCourse.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import Form from './Form';
+
+export default class CreateCourse extends Component {
+    state = {
+        title: '',
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: '',
+        errors: [],
+    }
+
+    change = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState(() => {
+            return {
+                [name]: value
+            }
+        });
+    }
+
+    submit = () => {
+        const {context} = this.props;
+        const {title, description, estimatedTime, materialsNeeded} = this.state;
+        const {
+            context: {
+                authenticatedUser: {
+                    user: {id, password, emailAddress},
+                },
+            },
+        } = this.props;
+
+        const courseInfo = { title, description, estimatedTime, materialsNeeded, userId: id, emailAddress }
+        context.actions.CreateCourse(courseInfo, emailAddress, password)
+                       .then((errors) => {
+                            this.setState((errors) => {
+                                if (errors.length) {
+                                    this.setState({errors});
+                                } else {
+                                    this.props.history.push('/');
+                                }
+                            });
+                       })
+                       .catch((error) => {
+                            this.props.history.push({
+                                pathname: '/error',
+                                state: {
+                                    error: error.message
+                                },
+                            });
+                        });
+    }
+
+    cancel = () => {
+        this.props.history.push('/');
+    }
+
+    render() {
+        const {title, description, estimatedTime, materialsNeeded, errors} = this.state;
+        const {
+            context: {
+                authenticatedUser: {user},
+            },
+        } = this.props;
+
+       return (
+            <div className='wrap'>
+                <h2>Create Course</h2>
+                <Form submit={this.submit} cancel={this.cancel} errors={errors} submitButtonText='Create Course' 
+                    elements={() => {
+                        <>
+                            <div className='main--flex'>
+                                <label htmlFor='courseTitle'>Course Title</label>
+                                <input id='CourseTitle' name='CourseTitle' type='text' value={title} onChange={this.change} />
+                                <p>By {user.firstName} {user.lastName}</p>
+
+                                <label htmlFor='courseDescription'>Course Description</label>
+                                <textarea id='courseDescription' name='courseDescription' value={description} onChange={this.change} />
+                            </div>
+                            <div>
+                                <label htmlFor='estimatedTime'>Estimated Time</label>
+                                <input id='estimatedTime' name='estimatedTime' type='text' value={estimatedTime} onChange={this.change} />
+
+                                <label htmlFor='materialsNeeded'>Materials Needed</label>
+                                <textarea id='materialsNeeded' name='materialsNeeded' value={materialsNeeded} onChange={this.change} />
+                            </div>
+                        </>
+                    }}
+                />
+            </div>
+       );
+    }
+}
