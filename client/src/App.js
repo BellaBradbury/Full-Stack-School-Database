c60b170088c97033e120e026f3067db4baa9fec3
BellaBradbury <bellabradbury@yahoo.com>
2023-02-15 16:10:22 -0500

    course routes added

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,20 @@ import NotFound from './components/NotFound';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
-import Authenticated from './components/Authenticated';
+import CreateCourse from './components/CreateCourse';
+import CourseDetail from './components/CourseDetail';
+import UpdateCourse from './components/UpdateCourse'
 
 import withContext from './Context';
 import PrivateRoute from './PrivateRoute';
 
 const HeaderWithContext = withContext(Header);
-const AuthWithContext = withContext(Authenticated);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
+const CourseCreateWithContext = withContext(CreateCourse);
+const CourseDetailWithContext = withContext(CourseDetail);
+const CourseUpdateWithContext = withContext(UpdateCourse);
 
 function App() {
     return(
@@ -29,14 +33,16 @@ function App() {
                 <HeaderWithContext />
                 <Routes>
                     <Route exact path='/' component={Courses} />
+                    <Route path='/courses/:id' component={CourseDetailWithContext} />
                     <Route path='/signUp' component={UserSignUpWithContext} />
                     <Route path='/signIn' component={UserSignInWithContext} />
                     <Route path='/signOut' component={UserSignOutWithContext} />
-                    <PrivateRoute path='/authenticated' component={AuthWithContext} />
-                    <Route path='/signin' component={UserSignInWithContext} />
-                    <Route path='/signup' component={UserSignUpWithContext} />
-                    <Route path='/signout' component={UserSignOutWithContext} />
-                    <Route component={NotFound} />
+                    <PrivateRoute path='/courses/create' component={CourseCreateWithContext} />
+                    <PrivateRoute path='/courses/:id/update' component={CourseUpdateWithContext} />
+                    <Route path='/notfound' component={NotFound} />
+                    <Route path='*' component={NotFound} />
+                    <Route path='/forbidden' />
+                    <Route path='/error' />
                 </Routes>
             </div>
         </Router>
