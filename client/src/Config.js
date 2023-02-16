cbc1dde857e429cebc61cddbaaafca6143fb894e
BellaBradbury <bellabradbury@yahoo.com>
2023-02-16 15:41:15 -0500

    es lint warnings disabled

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,4 +1,4 @@
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     apiBaseUrl: 'http://localhost:5000/api',
 };
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -21,7 +21,8 @@ export default function Courses({ history }) {
                     });
         }
         coursesFind();
-    }, [history, coursesData]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const courseTitles = coursesData.map((course) => {
         return (
@@ -33,17 +34,15 @@ export default function Courses({ history }) {
     });
 
     return (
-        <main>
-            <div className='wrap main--grid'>
-                {courseTitles}
-                <Link className='course--module course--add-module' to='/courses/create'>
-                    <span className='course--add--title'>
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
-                         <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                        </svg>
-                    New Course </span>
-                </Link>
-            </div>
-        </main>
+        <div className='wrap main--grid'>
+            {courseTitles}
+            <Link className='course--module course--add-module' to='/courses/create'>
+                <span className='course--add--title'>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                    </svg>
+                New Course </span>
+            </Link>
+        </div>
     )
 }
