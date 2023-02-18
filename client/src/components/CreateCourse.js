f543000ae49e9b1a21b6d3728f6efa97dde09c98
BellaBradbury <bellabradbury@yahoo.com>
2023-02-18 01:36:35 -0500

    create form appears

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -24,25 +24,24 @@ export default class CreateCourse extends Component {
     submit = () => {
         const {context} = this.props;
         const {title, description, estimatedTime, materialsNeeded} = this.state;
-        console.log(context);
+        
         const {
             context: {
                 authenticatedUser: {
-                    user: {id, password, emailAddress},
+                    id, password, emailAddress,
                 },
             },
         } = this.props;
 
         const courseInfo = { title, description, estimatedTime, materialsNeeded, userId: id, emailAddress }
+        console.log(courseInfo);
         context.actions.createCourse(courseInfo, emailAddress, password)
                        .then((errors) => {
-                            this.setState((errors) => {
                                 if (errors.length) {
                                     this.setState({errors});
                                 } else {
                                     this.props.history.push('/');
-                                }
-                            });
+                                };
                        })
                        .catch((error) => {
                             this.props.history.push({
@@ -60,12 +59,12 @@ export default class CreateCourse extends Component {
 
     render() {
         const {title, description, estimatedTime, materialsNeeded, errors} = this.state;
-
+        console.log(Form);
        return (
             <div className='wrap'>
                 <h2>Create Course</h2>
                 <Form submit={this.submit} cancel={this.cancel} errors={errors} submitButtonText='Create Course' 
-                    elements={() => {
+                    elements={() => (
                         <>
                             <div className='main--flex'>
                                 <label htmlFor='courseTitle'>Course Title</label>
@@ -83,9 +82,109 @@ export default class CreateCourse extends Component {
                                 <textarea id='materialsNeeded' name='materialsNeeded' value={materialsNeeded} onChange={this.change} />
                             </div>
                         </>
-                    }}
+                    )}
                 />
             </div>
        );
     }
 }
+
+
+
+
+
+
+
+
+
+// // IMPORTED FUNCTIONS & MODULES
+// import React, {Component} from 'react';
+// import Form from './Form';
+
+// export default class CreateCourse extends Component {
+//     state = {
+//         title: '',
+//         description: '',
+//         estimatedTime: '',
+//         materialsNeeded: '',
+//         errors: [],
+//     }
+
+//     change = (event) => {
+//         const name = event.target.name;
+//         const value = event.target.value;
+//         this.setState(() => {
+//             return {
+//                 [name]: value
+//             }
+//         });
+//     }
+
+//     submit = () => {
+//         const {context} = this.props;
+//         const {title, description, estimatedTime, materialsNeeded} = this.state;
+//         console.log(context);
+//         const {
+//             context: {
+//                 authenticatedUser: {
+//                     user: {id, password, emailAddress},
+//                 },
+//             },
+//         } = this.props;
+
+//         const courseInfo = { title, description, estimatedTime, materialsNeeded, userId: id, emailAddress }
+//         context.actions.createCourse(courseInfo, emailAddress, password)
+//                        .then((errors) => {
+//                             this.setState((errors) => {
+//                                 if (errors.length) {
+//                                     this.setState({errors});
+//                                 } else {
+//                                     this.props.history.push('/');
+//                                 }
+//                             });
+//                        })
+//                        .catch((error) => {
+//                             this.props.history.push({
+//                                 pathname: '/error',
+//                                 state: {
+//                                     error: error.message
+//                                 },
+//                             });
+//                         });
+//     }
+
+//     cancel = () => {
+//         this.props.history.push('/');
+//     }
+
+//     render() {
+//         const {title, description, estimatedTime, materialsNeeded, errors} = this.state;
+
+//        return (
+//             <div className='wrap'>
+//                 <h2>Create Course</h2>
+//                 <Form submit={this.submit} cancel={this.cancel} errors={errors} submitButtonText='Create Course' 
+//                     elements={() => {
+//                         <>
+//                             <div className='main--flex'>
+//                                 <label htmlFor='courseTitle'>Course Title</label>
+//                                 <input id='CourseTitle' name='CourseTitle' type='text' value={title} onChange={this.change} />
+//                                 <p>By {this.props.context.authenticatedUser.firstName} {this.props.context.authenticatedUser.lastName}</p>
+
+//                                 <label htmlFor='courseDescription'>Course Description</label>
+//                                 <textarea id='courseDescription' name='courseDescription' value={description} onChange={this.change} />
+//                             </div>
+//                             <div>
+//                                 <label htmlFor='estimatedTime'>Estimated Time</label>
+//                                 <input id='estimatedTime' name='estimatedTime' type='text' value={estimatedTime} onChange={this.change} />
+
+//                                 <label htmlFor='materialsNeeded'>Materials Needed</label>
+//                                 <textarea id='materialsNeeded' name='materialsNeeded' value={materialsNeeded} onChange={this.change} />
+//                             </div>
+//                         </>
+//                     }}
+//                 />
+//             </div>
+//        );
+//     }
+// }
