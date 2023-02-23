5a8db5c0670ae1b1c360f4c3c0c62a8c8390f9e3
BellaBradbury <bellabradbury@yahoo.com>
2023-02-23 16:08:44 -0500

    api url change

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,5 @@
 // EXPORTS API URL
     // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    apiBaseUrl: 'full-stack-app-wd-13-production.up.railway.app/api',
+    apiBaseUrl: 'full-stack-app-wd-13-production.up.railway.app/',
 };
